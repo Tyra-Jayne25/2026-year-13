a742bfd75ccedc03ec1b932d6e16336df1b36d41
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-23 14:49:33 +1300

    Create contact_V1.py

---
+++ Contacts activity/contact_V1.py
@@ -0,0 +1,13 @@
+contact = {"1":
+    {"First Name": "John",
+    "Last Name": "Smith",
+    "Mobile": "123-456-7890",
+    "Email": "johnsmith@example.com"},
+
+    "2":
+    {"First Name": "Jane",
+    "Last Name": "Rose",
+    "Mobile": "987-654-3210",
+    "Email": "janerose@example.com"}
+
+}
