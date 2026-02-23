d68de65072ba64ae7974cbcc4bd4db2104db40a1
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-23 14:52:48 +1300

    Update contact_V1.py

--- Contacts activity/contact_V1.py
+++ Contacts activity/contact_V1.py
@@ -1,13 +1,23 @@
 contact = {"1":
     {"First Name": "John",
     "Last Name": "Smith",
-    "Mobile": "123-456-7890",
+    "Mobile": "123 456 7890",
     "Email": "johnsmith@example.com"},
 
     "2":
     {"First Name": "Jane",
     "Last Name": "Rose",
-    "Mobile": "987-654-3210",
+    "Mobile": "987 654 3210",
     "Email": "janerose@example.com"}
 
 }
+print(contact["1"]["First Name"])
+print(contact["1"]["Last Name"])
+print(contact["1"]["Mobile"])
+print(contact["1"]["Email"])
+
+print(contact["2"]["First Name"])
+print(contact["2"]["Last Name"])
+print(contact["2"]["Mobile"])
+print(contact["2"]["Email"])
+
