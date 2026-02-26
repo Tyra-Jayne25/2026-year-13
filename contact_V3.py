96bec8a8880029bf3cf4ac7a85663399f35e7b53
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-27 10:34:52 +1300

    Update contact_V3.py

--- contact_V3.py
+++ contact_V3.py
@@ -18,50 +18,51 @@ for i in range (2):
 
 print(NAME)
 
-choice = input("Do you want to: (a)dd a contact, (S)earch a contact, (e)dit a contact, (d)elete a contact, or (p)rint all contacts? ")
-if choice == "a":
-    ID = input("\n\nPerson ID: ")
-    NAME[ID] = {}
+while True:
+    choice = input("Do you want to: (a)dd a contact, (S)earch a contact, (e)dit a contact, (d)elete a contact, (p)rint all contacts, or (q)uit? ")
+    if choice == "a":
+        ID = input("\n\nPerson ID: ")
+        NAME[ID] = {}
 
-    First = input("\n\nFirst Name: ")
-    NAME[ID]["First Name"] = First
+        First = input("\n\nFirst Name: ")
+        NAME[ID]["First Name"] = First
 
-    Last = input("\n\nLast Name: ")
-    NAME[ID]["Last Name"] = Last
+        Last = input("\n\nLast Name: ")
+        NAME[ID]["Last Name"] = Last
 
-    Mobile = input("\n\nMobile: ")
-    NAME[ID]["Mobile"] = Mobile
+        Mobile = input("\n\nMobile: ")
+        NAME[ID]["Mobile"] = Mobile
 
+        Email = input("\n\nEmail: ")
+        NAME[ID]["Email"] = Email
 
-    Email = input("\n\nEmail: ")
-    NAME[ID]["Email"] = Email
+    elif choice == "s":
+        ID = input("\n\nPerson ID: ")
+        print(NAME[ID])
 
-elif choice == "s":
-    ID = input("\n\nPerson ID: ")
-    print(NAME[ID])
-elif choice == "e":
-    ID = input("\n\nPerson ID: ")
-    First = input("\n\nFirst Name: ")
-    NAME[ID]["First Name"] = First
+    elif choice == "e":
+        ID = input("\n\nPerson ID: ")
+        First = input("\n\nFirst Name: ")
+        NAME[ID]["First Name"] = First
 
-    Last = input("\n\nLast Name: ")
-    NAME[ID]["Last Name"] = Last
+        Last = input("\n\nLast Name: ")
+        NAME[ID]["Last Name"] = Last
 
-    Mobile = input("\n\nMobile: ")
-    NAME[ID]["Mobile"] = Mobile
+        Mobile = input("\n\nMobile: ")
+        NAME[ID]["Mobile"] = Mobile
 
-    Email = input("\n\nEmail: ")
-    NAME[ID]["Email"] = Email
+        Email = input("\n\nEmail: ")
+        NAME[ID]["Email"] = Email
 
-elif choice == "d":
-    ID = input("\n\nPerson ID: ")
-    del NAME[ID]
+    elif choice == "d":
+        ID = input("\n\nPerson ID: ")
+        del NAME[ID]
 
-elif choice == "p":
-    print(NAME)
+    elif choice == "p":
+        print(NAME)
 
+    elif choice == "q":
+        print("Goodbye!")
+        break
 
-
-
-    
-    
+print("==================================")
