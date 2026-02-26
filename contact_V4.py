7ee15de11ac9c170fc425357961c624357450645
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-27 10:50:15 +1300

    Create contact_V4.py

---
+++ contact_V4.py
@@ -0,0 +1,83 @@
+NAME = {}
+
+for i in range (2):
+    ID = input("\n\nPerson ID: ")
+    NAME[ID] = {}
+
+    First = input("\n\nFirst Name: ")
+    NAME[ID]["First Name"] = First
+
+    Last = input("\n\nLast Name: ")
+    NAME[ID]["Last Name"] = Last
+
+    Mobile = input("\n\nMobile: ")
+    NAME[ID]["Mobile"] = Mobile
+
+    Email = input("\n\nEmail: ")
+    NAME[ID]["Email"] = Email
+
+print(NAME)
+
+while True:
+    choice = input("Do you want to: (a)dd a contact, (S)earch a contact, (e)dit a contact, (d)elete a contact, (p)rint all contacts, or (q)uit? ")
+    if choice == "a":
+        ID = input("\n\nPerson ID: ")
+        NAME[ID] = {}
+
+        First = input("\n\nFirst Name: ")
+        NAME[ID]["First Name"] = First
+
+        Last = input("\n\nLast Name: ")
+        NAME[ID]["Last Name"] = Last
+
+        Mobile = input("\n\nMobile: ")
+        NAME[ID]["Mobile"] = Mobile
+
+        Email = input("\n\nEmail: ")
+        NAME[ID]["Email"] = Email
+
+    elif choice == "s":
+        search_term = input("\n\n:First Name or Last Name: ")
+        found = False 
+        for ID in NAME:
+            if search_term in NAME[ID]["First Name"] or search_term in NAME[ID]["Last Name"]:
+                print("\n\nPerson ID:", ID)
+                print("First Name:", NAME[ID]["First Name"])
+                print("Last Name:", NAME[ID]["Last Name"])
+                print("Mobile:", NAME[ID]["Mobile"])
+                print("Email:", NAME[ID]["Email"])
+                found = True
+        if not found:
+            print("No contacts found.")
+
+    elif choice == "e":
+        ID = input("\n\nPerson ID: ")
+        First = input("\n\nFirst Name: ")
+        NAME[ID]["First Name"] = First
+
+        Last = input("\n\nLast Name: ")
+        NAME[ID]["Last Name"] = Last
+
+        Mobile = input("\n\nMobile: ")
+        NAME[ID]["Mobile"] = Mobile
+
+        Email = input("\n\nEmail: ")
+        NAME[ID]["Email"] = Email
+
+    elif choice == "d":
+        ID = input("\n\nPerson ID: ")
+        del NAME[ID]
+
+    elif choice == "p":
+        for ID in NAME:
+            print("\n\nPerson ID:", ID)
+            print("First Name:", NAME[ID]["First Name"])
+            print("Last Name:", NAME[ID]["Last Name"])
+            print("Mobile:", NAME[ID]["Mobile"])
+            print("Email:", NAME[ID]["Email"])
+
+    elif choice == "q":
+        print("Goodbye!")
+        break
+
+print("==================================")
