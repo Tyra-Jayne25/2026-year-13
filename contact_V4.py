f06ea8dce8d8357735c52ef7269ea8d3fa7adf22
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-27 10:53:21 +1300

    Update contact_V4.py

--- contact_V4.py
+++ contact_V4.py
@@ -18,6 +18,7 @@ for i in range (2):
 
 print(NAME)
 
+#Menu loop to add, search, edit, delete, print, or quit
 while True:
     choice = input("Do you want to: (a)dd a contact, (S)earch a contact, (e)dit a contact, (d)elete a contact, (p)rint all contacts, or (q)uit? ")
     if choice == "a":
@@ -36,8 +37,9 @@ while True:
         Email = input("\n\nEmail: ")
         NAME[ID]["Email"] = Email
 
+#Changing the search function to search by first name or last name instead of ID"
     elif choice == "s":
-        search_term = input("\n\n:First Name or Last Name: ")
+        search_term = input("\n\nFirst Name or Last Name: ")
         found = False 
         for ID in NAME:
             if search_term in NAME[ID]["First Name"] or search_term in NAME[ID]["Last Name"]:
