fd583d3c2f977abc0212c628250a4a6985631e1e
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-03-06 14:49:26 +1300

    Update contact_V6.py

--- contact_V6.py
+++ contact_V6.py
@@ -1,76 +1,6 @@
 #Adding buttons to the program using pygame
 #adding buttons for add, search, edit, delete, print, and quit functions
 
-import pygame
-pygame.init()
-
-# Set colour variables
-light_grey=(200,200,200)
-lightish_grey=(180,180,180)
-dark_grey=(100,100,100)
-darker_grey=(70,70,70)
-blue=(0,0,255)
-black=(0,0,0)
-white=(255,255,255)
-
-# Set up the display window
-screen = pygame.display.set_mode([500, 550])
-
-# Intialise button 1 variables
-x1_pos=100
-y1_pos=150
-rec1_colour=light_grey
-button1=pygame.Rect(x1_pos,y1_pos,80,80)
-# Intialise button 2 variables
-x2_pos=300
-y2_pos=150
-rec2_colour=light_grey
-button2=pygame.Rect(x2_pos,y2_pos,80,80)
-
-running = True
-click=0
-
-while running:
-    screen.fill((darker_grey))
-
-    # When the user clicks x in the game window
-    for event in pygame.event.get():
-        mouse_x,mouse_y=pygame.mouse.get_pos()
-        if event.type == pygame.QUIT:
-            running=False
-
-        # When the user clicks on a button, animate the button and change its colour
-        elif event.type==pygame.MOUSEBUTTONDOWN:
-            if button1.collidepoint(event.pos):
-                rec1_colour=white
-                x1_pos=x1_pos+3
-                y1_pos=y1_pos+3
-                button1=pygame.Rect(x1_pos,y1_pos,80,80)
-            elif button2.collidepoint(event.pos):
-                rec2_colour=white
-                x2_pos=x2_pos+3
-                y2_pos=y2_pos+3
-                button2=pygame.Rect(x2_pos,y2_pos,80,80)
-            
-
-        # When the user releases the button, reset button to original colour and position
-        elif event.type==pygame.MOUSEBUTTONUP:
-            if button1.collidepoint(event.pos):
-                rec1_colour=light_grey
-                x1_pos=100
-                y1_pos=150
-                button1=pygame.Rect(x1_pos,y1_pos,80,80)
-            elif button2.collidepoint(event.pos):
-                rec2_colour=light_grey
-                x2_pos=300
-                y2_pos=150
-                button2=pygame.Rect(x2_pos,y2_pos,80,80)
-    # Draw the buttons
-    pygame.draw.rect(screen, rec1_colour, button1)
-    pygame.draw.rect(screen, rec2_colour, button2)
-    pygame.display.flip()
-pygame.quit()
-exit()
 
 #adding in the functions
 def add_contact(NAME):
@@ -146,30 +76,36 @@ for i in range (2):
 
 print(NAME)
 
-#Menu loop to add, search, edit, delete, print, or quit
-while True:
-    choice = input("Do you want to: (a)dd a contact, (S)earch a contact, (e)dit a contact, (d)elete a contact, (p)rint all contacts, or (q)uit? ")
+#adding the buttons using pygame
+import pygame
+pygame.init()
 
-#Using the functions instead of repeating code
-    if choice == "a":
-        add_contact(NAME)
+#colour variables
+light_grey=(200,200,200)
+lightish_grey=(180,180,180)
+dark_grey=(100,100,100)
+darker_grey=(70,70,70)
+blue=(0,0,255)
+black=(0,0,0)
+white=(255,255,255)
 
-#Changing the search function to search by first name or last name instead of ID"
-    elif choice == "s":
-        search_contact(NAME)
+#window
+screen = pygame.display.set_mode([500, 550])
+pygame.display.set_caption("Contact Manager")
 
-    elif choice == "e":
-        edit_contact(NAME)
+#font
+font = pygame.font.SysFont(None, 30)
 
-    elif choice == "d":
-        delete_contact(NAME)
+#button
+button_add = pygame.Rect(50, 50, 100, 50)
+button_search = pygame.Rect(200, 50, 100, 50)
+button_edit = pygame.Rect(350, 50, 100, 50)
+button_delete = pygame.Rect(50, 150, 100, 50)
+button_print = pygame.Rect(200, 150, 100, 50)
+button_quit = pygame.Rect(350, 150, 100, 50)
 
-    elif choice == "p":
-        print_contacts(NAME)
 
-#Adding a quit option to exit the program
-    elif choice == "q":
-        print("Goodbye!")
-        break
 
-print("==================================")
+
+
+
